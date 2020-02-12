0820b6080b11db1bf13c11172465adab32f8806f
Alexander Beaver <alex@beaver.dev>
2020-02-12 09:07:56 -0800

    Add Alpha CORS

--- Scouting2020/match/src/components/global/RunnerPage.js
+++ Scouting2020/match/src/components/global/RunnerPage.js
@@ -3,11 +3,33 @@ import React, {Component} from "react";
 import "material-design-lite/material.min";
 import "material-design-lite/material.min.css";
 import "../../assets/css/materialIcons.css";
+import EventListener, {withOptions} from 'react-event-listener';
+
 export default class RunnerPage extends Component{
+    configURL = "https://storage.googleapis.com/alpha.cdn.atco.mp/ScoutingGenerationSchema.json";
+
+
+    componentDidMount() {
+       this.updateConfig()
+    }
+
+    updateConfig(){
+        fetch(this.configURL)
+            .then(response => response.json())
+            .then(data => this.setState({ config:data }))
+            .then(console.log(this.state));
+
+    }
+    handleKeyPress = (event) => {
+        console.log(event.key);
+    }
+
     render(){
         return(
             <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
             mdl-layout--fixed-header">
+                <EventListener target={"window"} onKeyPress={this.handleKeyPress}/>
+
                 <header class="mdl-layout__header">
                     <div class="mdl-layout__header-row">
                         <div class="mdl-layout-spacer"></div>
